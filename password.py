6e64038507d5443d7913f0c2c81b9f6c24eb9f31
Cesar Chavez <cesar@ochohaikus.com>
2016-10-11 00:25:10 -0500

    Added some imput logic, a function to generate an array of passwords and some options for the command line

--- password.py
+++ password.py
@@ -2,8 +2,9 @@
 # Imports
 
 import random
+import sys
 
-def generator(complexity):
+def password_gen(complexity):
     #This function generate the string with the password
 
     #We open the local dictionary (on Linux systems)
@@ -20,8 +21,38 @@ def generator(complexity):
         word = word.translate(None, "'")
         word = word.title()
         password += word
+        if count != complexity-1:
+            password += "|"
         count += 1
 
     return password
 
-print(generator(3))
+def options_gen(complexity):
+    options = []
+    count = 0
+
+    while count < 3:
+        password=password_gen(complexity)
+        options.append(password)
+        count += 1
+
+    return options
+
+def iteraction():
+    #This function interacts with the user and use the generator function
+    complexity = int(input("Que tantas palabras debe de tener el password? "))
+    print("Tus 3 opciones son:")
+    options=options_gen(complexity)
+    print(options)
+    election=input("Escribe el numero de la opcion deseada y aprieta enter ")
+    print("Tu password es: "+str(options[election-1]))
+
+if len(sys.argv) >= 2:
+    foo = str(sys.argv[1])
+else:
+    foo = ""
+
+if foo == "options":
+    iteraction()
+else:
+    print(password_gen(2))
